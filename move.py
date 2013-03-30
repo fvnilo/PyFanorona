310bd1c3a4c32eaf1135d6d962c3cbf48fcf8d07
Nylo Gavins <ny.fanilo@gmail.com>
2013-03-30 01:56:44 -0400

    Added comments in the move generator

--- move.py
+++ move.py
@@ -35,25 +35,30 @@ class Move:
     RIGHT = 1
     
     # Legal move dictionary
-    legal_moves = { "UL" : UP + LEFT, "U" : UP, "UR" : UP + RIGHT, "DL" : DOWN + LEFT, "D" : DOWN, "DR" : DOWN + RIGHT, "R" : RIGHT, "L" : LEFT }
+    LEGAL_MOVES = { "UL" : UP + LEFT, "U" : UP, "UR" : UP + RIGHT, "DL" : DOWN + LEFT, "D" : DOWN, "DR" : DOWN + RIGHT, "R" : RIGHT, "L" : LEFT }
     
+    # Constants
+    BOARD_LOW_LIMIT = 0
+    BOARD_UP_LIMIT = 44
+
     @classmethod
     def is_legal_move(cls, start, end, attack, defense):
         """Checks whether a move is legal or not"""
         
-        if end < 0 or end >= 45:
+        if end < cls.BOARD_LOW_LIMIT or end > cls.BOARD_UP_LIMIT:
             return False;
         
-        move = end - start
+        move = end - start # The length of the move
         correct_move = True
         
-        legal_move = move in cls.legal_moves.values()
-        piece_placed = attack & 1 << start == (1 << start)
+        legal_move = move in cls.LEGAL_MOVES.values() # Verify that the move is legal.
+        piece_placed = attack & 1 << start == (1 << start) # Player did select a piece from his side.
         
         # Mathematically speaking, from a zero-index position, only even pieces can move diagonally    
         if move not in [cls.UP, cls.DOWN, cls.LEFT, cls.RIGHT]:
             correct_move = start % 2 == 0
-            
+        
+        # There is no piece on the spot the player wants to go to.
         end_free = attack & 1 << end == 0 and defense & 1 << end == 0
         
         return legal_move and piece_placed and end_free and correct_move
@@ -62,6 +67,7 @@ class Move:
     def apply_attack_move(cls, start, end, board):
         """Applies the offensive move"""
         
+        # Toggling bits
         board ^= 1 << start
         board ^= 1 << end
             
@@ -70,20 +76,21 @@ class Move:
     @classmethod
     def apply_defense_move(cls, start, end, board):
         """Applies the defensive move"""
-        mult = 2
+        mult = 2 # First round, multiply by two in the direction
         move = end - start
         withdrawal = False
         shift = cls._calc_shift(start, move, mult)
             
-        if not board & 1 << shift == (1 << shift): # withdrawal move
+        # Is it a withdrawal move ? 
+        if not board & 1 << shift == (1 << shift):
             withdrawal = True
             move = -move
             shift = cls._calc_shift(end, move, mult)
         
-    
+        # As long as there is a piece in the line of attack and that we can virtually continue.
         while board & 1 << shift == (1 << shift) and cls.must_continue(start, shift, move):
-            board ^= 1 << shift
-            mult +=1
+            board ^= 1 << shift # Toggle bit
+            mult += 1 # Next round, check one step further.
             if withdrawal:
                 shift = cls._calc_shift(end, move, mult)
             else:
@@ -93,13 +100,15 @@ class Move:
     
     @classmethod
     def must_continue(cls, start, shift, move):
-        """Check whether captures can continue or not."""
-        if move == cls.LEFT or move == cls.RIGHT:
-            return start // 9 == shift // 9
-        
+        """Check whether captures can continue or not from a bitmap point of view"""
         if move == cls.UP or move == cls.DOWN:
             return True
+
+        if move == cls.LEFT or move == cls.RIGHT:
+            return start // 9 == shift // 9 # Check that we are still in the same row
         
+        # When moving diagonally (UR or DL), you could be toggling the bit on index 0
+        # otherwise the move might have come back to the bit 0 and should not toggle it.
         return (shift == 0 and start % 10 == 0) or shift != 0
         
     
@@ -107,7 +116,9 @@ class Move:
     def _calc_shift(cls, start, move, mult):
         """Calculates the shift for applying the defensive move"""
         shift = start + (move * mult)
+
+        # Reverse the shift
         if shift < 0: 
-            shift = 44 - shift
+            shift = cls.BOARD_UP_LIMIT - shift
             
         return shift
